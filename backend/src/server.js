43c34e02f2c57db5dd81b990cb5e88c5cc450de7
Соронбаев Кутман <justkutmans@gmail.com>
2026-02-02 12:06:04 +0600

    post route added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,37 @@
 import express from "express";
-import {ENV} from "./config/env.js";
-import {connectDB} from "./config/db.js";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 import cors from "cors";
-import {clerkMiddleware} from "@clerk/express";
+import { clerkMiddleware } from "@clerk/express";
 import userRoute from "./routes/user.route.js";
-
+import postRoute from "./routes/post.route.js";
 
 const app = express();
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
-app.get("/", (req, res) => res.send("hello from server"))
+app.get("/", (req, res) => res.send("hello from server"));
 
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
-app.use("/api/users", userRoute)
+//error handling middleware
+
+app.use((err, req, res) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: err.message || "Internal server error" });
+});
 
 const startServer = async () => {
-    try {
-        await connectDB()
-        app.listen(ENV.PORT, () => console.log(`Server is running ${ENV.PORT}`))
-    } catch(e) {
-        console.error("failed to start server:", e.message);
-        process.exit(1)
-    }
-}
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log(`Server is running ${ENV.PORT}`));
+  } catch (e) {
+    console.error("failed to start server:", e.message);
+    process.exit(1);
+  }
+};
 
-startServer()
+startServer();
